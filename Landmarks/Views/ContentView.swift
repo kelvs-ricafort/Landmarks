5ec61ca5f2ddf8c40f428b26ea23a209503f82b1
Kelvin Ricafort <kelvs.r@outlook.com>
2023-05-19 00:13:04 +0800

    Handling User Input

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kelvin on 5/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
