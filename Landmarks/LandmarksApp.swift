7a5aa2e619fdb67c6dcae775606f05b289c0b52f
Kelvin Ricafort <kelvs.r@outlook.com>
2023-05-18 16:23:19 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kelvin on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
